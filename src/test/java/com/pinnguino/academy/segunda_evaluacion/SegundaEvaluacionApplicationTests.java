11a920dedd51d988fd10e7a6628e7624ae1660e5
pinnguino <s.gallardogaston@gmail.com>
2025-10-27 10:25:02 -0300

    Primer commit
    Creación del proyecto en Spring Boot

---
+++ src/test/java/com/pinnguino/academy/segunda_evaluacion/SegundaEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinnguino.academy.segunda_evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundaEvaluacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
