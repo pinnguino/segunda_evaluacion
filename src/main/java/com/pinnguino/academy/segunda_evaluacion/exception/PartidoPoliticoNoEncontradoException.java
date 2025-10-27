2e6fd9eb3270f38cd0123d732e055339feed6bf0
pinnguino <s.gallardogaston@gmail.com>
2025-10-27 16:30:23 -0300

    Add exception for PartidoPolitico
    Added the PartidoPoliticoNoEncontradoException for the
    PartidoPoliticoService.

---
+++ src/main/java/com/pinnguino/academy/segunda_evaluacion/exception/PartidoPoliticoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.pinnguino.academy.segunda_evaluacion.exception;
+
+public class PartidoPoliticoNoEncontradoException extends Exception {
+    public PartidoPoliticoNoEncontradoException(Long id) {
+        super("Partido con ID " + id + " no encontrado");
+    }
+}
