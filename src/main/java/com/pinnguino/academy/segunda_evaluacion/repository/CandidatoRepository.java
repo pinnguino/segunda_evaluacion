3bd79599bc558cb3bef6071ae29642b8a50d5432
pinnguino <s.gallardogaston@gmail.com>
2025-10-27 18:18:54 -0300

    Fix CandidatoRepository data type
    Fixed the JPA repository with the correct data type.

--- src/main/java/com/pinnguino/academy/segunda_evaluacion/repository/CandidatoRepository.java
+++ src/main/java/com/pinnguino/academy/segunda_evaluacion/repository/CandidatoRepository.java
@@ -1,9 +1,9 @@
 package com.pinnguino.academy.segunda_evaluacion.repository;
 
-import com.pinnguino.academy.segunda_evaluacion.model.PartidoPolitico;
+import com.pinnguino.academy.segunda_evaluacion.model.Candidato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CandidatoRepository extends JpaRepository<PartidoPolitico, Long> {
+public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
 }
